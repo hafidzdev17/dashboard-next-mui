9fb0c98c4ba08edf611d7ecde76fb52314c04914
hafidzmasruri17 <do.crazy192@gmail.com>
2023-03-29 10:17:44 +0700

    feat: update config route in sidenav dashboard

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,84 +1,55 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
-import { SvgIcon } from '@mui/material';
+import ChartBarIcon from "@heroicons/react/24/solid/ChartBarIcon";
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
+import ProductsIcon from "@heroicons/react/24/solid/ShoppingBagIcon";
+import { SvgIcon } from "@mui/material";
+import { PATH_DASHBOARD as dashboard } from "../../routes";
 
 export const items = [
   {
-    title: 'Overview',
-    path: '/',
+    title: "Overview",
+    path: dashboard.root,
     icon: (
       <SvgIcon fontSize="small">
         <ChartBarIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Customers',
-    path: '/customers',
+    title: "Products",
+    path: dashboard.products,
+    icon: (
+      <SvgIcon fontSize="small">
+        <ProductsIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Customers",
+    path: dashboard.customers,
     icon: (
       <SvgIcon fontSize="small">
         <UsersIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Companies',
-    path: '/companies',
-    icon: (
-      <SvgIcon fontSize="small">
-        <ShoppingBagIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Account',
-    path: '/account',
+    title: "Account",
+    path: dashboard.account,
     icon: (
       <SvgIcon fontSize="small">
         <UserIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Settings',
-    path: '/settings',
+    title: "Settings",
+    path: dashboard.settings,
     icon: (
       <SvgIcon fontSize="small">
         <CogIcon />
       </SvgIcon>
-    )
+    ),
   },
-  {
-    title: 'Login',
-    path: '/auth/login',
-    icon: (
-      <SvgIcon fontSize="small">
-        <LockClosedIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Register',
-    path: '/auth/register',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserPlusIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Error',
-    path: '/404',
-    icon: (
-      <SvgIcon fontSize="small">
-        <XCircleIcon />
-      </SvgIcon>
-    )
-  }
 ];
